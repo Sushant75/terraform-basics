9cc409eb1ca34cb3cc692f67fa1f32dc06134415
Sushant Chandra Kalia <sushantchandrakalia@gmail.com>
2022-10-02 14:59:11 +0530

    Terraform Command Basics

--- terraform-command-basics/ec2-instance.tf
+++ terraform-command-basics/ec2-instance.tf
@@ -1,25 +1,25 @@
-# Terraform Block
+# Terraform Settings Block
 
 terraform {
-  required_version = "~> 1.2.9"
+  required_version = "~> 1.3.1"
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 4.0"
+        version = "~> 4.33.0"
     }
   }
 }
 
-# Providers Block
+# Provider Block
 
 provider "aws" {
   profile = "default"
   region = "us-east-1"
 }
 
-# AWS EC2 Instance
+# Resource Block
 
 resource "aws_instance" "ec2demo" {
-  ami = "ami-05fa00d4c63e32376"
-  instance_type = "t2.micro"
+    ami = "ami-026b57f3c383c2eec"
+    instance_type = "t2.micro"
 }
