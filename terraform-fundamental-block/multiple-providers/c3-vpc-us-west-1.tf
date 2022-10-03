7d1265b0f850967d333b99db37fdbad708728315
Sushant Chandra Kalia <sushantchandrakalia@gmail.com>
2022-10-03 17:16:30 +0530

    terraform-fundamental-block

---
+++ terraform-fundamental-block/multiple-providers/c3-vpc-us-west-1.tf
@@ -0,0 +1,11 @@
+########### Resource Block ###############
+
+### Resource-1: Create VPC in us-west-1 ###
+
+resource "aws_vpc" "vpc-us-west-1" {
+  cidr_block = "10.0.0.0/16"
+  provider = aws.aws-west-1
+  tags = {
+    "Name" = "vpc-us-west-1"
+  }
+}
