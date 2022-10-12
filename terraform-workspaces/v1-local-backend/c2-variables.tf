1879d71acead23204df718fd4b8ed21fb2251e82
Sushant Chandra Kalia <sushantchandrakalia@gmail.com>
2022-10-12 09:25:46 +0530

    terraform-workspaces

---
+++ terraform-workspaces/v1-local-backend/c2-variables.tf
@@ -0,0 +1,20 @@
+# Input Variables
+
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+# variable "ec2_ami_id" {
+#   description = "AMI ID"
+#   type = string 
+#   default = "ami-026b57f3c383c2eec"
+# }
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  #default     = "t2.micro"
+  default = "t2.small"
+}
